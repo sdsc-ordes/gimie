70f4f29cdc06db2b15c1db7d96afbe2759930902
rmfranken <77491494+rmfranken@users.noreply.github.com>
2022-12-20 11:56:35 +0100

    feat: Added triple serialization of license result (spdx url)

--- gimie/sources/license.py
+++ gimie/sources/license.py
@@ -17,33 +17,42 @@
 import os
 from scancode.api import get_licenses
 import re
+from rdflib import Graph, Literal, URIRef
 
 path = r"C:\Users\franken\gimie"  # this is to be filled with the path from the CLI input, not sure how to access it
 
 
 def find_licenses(path: str):
-    """returns a python list of licenses found at destination"""
+    """returns a python list of licenses found at destination path"""
     path_files = os.listdir(path)
-    print(path)
-    print(path_files)
+    g = Graph()
     found_licenses = []
     for file in path_files:
-        result = re.match("licens", file, flags=re.IGNORECASE)
-        #regex used is very basic right now, what are other common license file names?
+        result = re.match(
+            "((licens.*)|(reuse)|(copy))", file, flags=re.IGNORECASE
+        )
+        # regex used is very basic right now, what are other common license file names?
         if result:
             license_location = str(path) + "\\" + str(file)
             license_mappings = get_licenses(license_location, min_score=50)
             # todo we need some tests to see how to set this min_score param
-            found_licenses.append(
-                ((license_mappings.get("licenses")[0]).get("key"))
-            )
 
+            extracted_license = (license_mappings.get("licenses")[0]).get(
+                "spdx_url"
+            )
+            found_licenses.append(extracted_license)
+            g.add(
+                (
+                    URIRef("https://www.gimie.org/repo"),
+                    URIRef("https://schema.org/license"),
+                    URIRef(str(extracted_license)),
+                )
+            )
+            g.serialize(destination="extractedtriple.ttl", format="ttl")
     return found_licenses
 
-    # todo had to remove file "html" from gimie because it was matching the package html
 
-print(find_licenses(path))
-
+# todo write a similar function for online repositories using just the repository URL
 
 # class LicenseMetadata:
 #     def __init__(self, path: str):
