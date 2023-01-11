d56b88b06409f9eb53915df41b7a278c38c8e550
rmfranken <77491494+rmfranken@users.noreply.github.com>
2023-01-11 08:22:39 +0100

    refactor: added documentation, refactored the generate_fair_uri function to accept a to-be-made "is_online function"

--- gimie/make_graph.py
+++ gimie/make_graph.py
@@ -6,59 +6,69 @@ from calamus.schema import JsonLDSchema
 schema = fields.Namespace("http://schema.org/")
 
 
-class Software:
-    def __init__(self, repo_uri: str):
-        self._id = repo_uri
-        self.license_url = "http://example.com/books/1"  # LicenseMetadata(self.path).get_licenses() TODO rm
-        self.language = (
-            "python"  # FilesMetadata(self.path).get_programming_lang() TODO rm
-        )
-
-
-class SoftwareSchema(JsonLDSchema):
-    _id = fields.Id()
-    license_url = fields.String(schema.license)
-    language = fields.String(schema.progLang)
-
-    class Meta:
-        rdf_type = schema.SoftwareApplication
-        model = Software
-
-
-def to_graph(repository_location, output_format="ttl"):
-    software_instance = Software(repository_location)
-    jsonld_dict = SoftwareSchema().dump(software_instance)
-    g = Graph()
-    g.parse(format="json-ld", data=jsonld_dict)
-
-    # prefix assignment, still have to code the automation of this
-    g.bind("schema", schema)
-    print(g.serialize(format=output_format))
-
-
-# TODO add function and class descriptions, write tests, see how license_to graph and fair uri functions fit into this
-
-
-def generate_fair_uri(repository_name):
+def generate_fair_uri(repository_path):
+    """given a repository_path, returns a URI with a hash for uniqueness, or the repository URL if it's online"""
     # Compute the SHA-256 hash of the repository name
-    hash = hashlib.sha256(repository_name.encode()).hexdigest()
-
-    # Return the FAIR URI in the form "fair:sha256-HASH, truncated to 5 characters to promote readability"
-    fair_uri = f"fair:{repository_name}_" + hash[:5]
+    hash = hashlib.sha256(repository_path.encode()).hexdigest()
+    # TODO https://www.github.com/repoX should return the same result as github.com/repoX
+    if (
+        False
+    ):  # repository_path.is_online(): #we need a method that checks whether the repopath is online or a foldername
+        fair_uri = repository_path
+    else:
+        # Return the FAIR URI in the form "fair:sha256-HASH, truncated to 5 characters to promote readability"
+        fair_uri = (
+            f"gimie:{repository_path}/" + hash[:5]
+        )  # TODO decide on URI we want to use for non online repos
     return fair_uri
 
 
-def license_to_graph(location: str, license: list):
+class Repository:
+    """A class to represent a GIT repository
+
+    Parameters
+    ----------
+    path: str
+        The path to the git repository.
+
+    Attributes
+    ----------
+    license_url (conform SPDX)
+    programming_language
+    """
+
+    def __init__(self, path: str):
+        self._id = generate_fair_uri(path)
+        self.license_url = "http://example.com/books/1"  # LicenseMetadata(self.path).get_licenses() TODO replace
+        self.programming_language = "python"  # FilesMetadata(self.path).get_programming_lang() TODO replace
+
+
+class RepositorySchema(JsonLDSchema):
+    """A Class which indicates how the attributes of Repository map to schema.org properties conform Calamus"""
+
+    _id = fields.Id()
+    license_url = fields.String(schema.license)
+    programming_language = fields.String(schema.programmingLanguage)
+
+    class Meta:
+        """a metaclass which indicates how the repository should be rdf:typed conform Calamus"""
+
+        rdf_type = schema.SoftwareApplication
+        model = Repository
+
+
+def to_graph(path, output_format="ttl"):
+    """A function which turns a given path into a graph in a desired rdfLib supported rdf serialization"""
+    software_instance = Repository(path)
+    jsonld_dict = RepositorySchema().dump(software_instance)
     g = Graph()
-    if "http://" or "https://" in location:
-        fair_uri = location
-    else:
-        fair_uri = generate_fair_uri(location)
-    for element in license:
-        g.add(
-            (
-                URIRef(fair_uri),
-                URIRef("https://schema.org/license"),
-                URIRef(str(element)),
-            )
-        )
+    g.parse(format="json-ld", data=jsonld_dict)
+
+    g.bind(
+        "schema", schema
+    )  # TODO prefix assignment, still have to figure out code for automation of this
+    print(g.serialize(format=output_format))
+
+
+# to_graph("https://wwww.gimie.com") #Test functionality
+# TODO add function and class descriptions, write tests, see how license_to graph and fair uri functions fit into this
