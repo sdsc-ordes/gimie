0bb3bc474d94b0492bfa0207c42bb0d56dab998c
cmdoret <cyril.mattheydoret@gmail.com>
2023-01-17 19:50:15 +0100

    refactor: central definitions in gimie.namespaces

---
+++ gimie/namespaces.py
@@ -0,0 +1,7 @@
+
+from rdflib.namespace import Namespace
+
+SDO = Namespace('http://schema.org')
+COD = Namespace('https://doi.org/10.5063/schema/codemeta-2.0')
+SD = Namespace('https://w3id.org/okn/o/sd/1.9.0')
+BIO = Namespace('https://bioschemas.org/')
