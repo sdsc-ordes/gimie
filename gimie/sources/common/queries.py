89b97f2a43e7b5e45324f5e162440bb28b184b5f
cmdoret <cyril.mattheydoret@gmail.com>
2023-05-31 10:21:49 +0200

    refactor(queries): rm redundant graphql query wrapper

--- gimie/sources/common/queries.py
+++ gimie/sources/common/queries.py
@@ -1,10 +1,10 @@
 import requests
-from typing import Any, Dict
+from typing import Any, Dict, List, Union
 
 
 def send_rest_query(
     api: str, query: str, headers: Dict[str, str]
-) -> Dict[str, Any]:
+) -> Union[List[Dict[str, Any]], Dict[str, Any]]:
     """Generic function to send a query to the GitHub/GitLab rest API."""
     resp = requests.get(
         url=f"{api}/{query}",
@@ -32,22 +32,3 @@ def send_graphql_query(
     if resp.status_code != 200:
         raise ConnectionError(resp.json()["message"])
     return resp.json()
-
-
-def query_graphql(
-    api: str, repo_query: str, data: dict, headers: Dict[str, str]
-) -> Dict[str, Any]:
-    """Queries the GitHub GraphQL API to extract metadata about
-    target repository.
-
-    Parameters
-    ----------
-    api:
-        URL for GraphQL API
-    repo_query:
-        query defining what information should be retrieved from the GraphQL API.
-    data:
-        parameters of the query
-    """
-    repo = send_graphql_query(api, repo_query, data=data, headers=headers)
-    return repo
