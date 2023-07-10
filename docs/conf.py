e3361ad7e3985f994eb48dd20d2c0ca3eb650d4a
cmdoret <cyril.mattheydoret@gmail.com>
2023-07-10 18:19:11 +0200

    chore: bump to 0.5.1

--- docs/conf.py
+++ docs/conf.py
@@ -9,7 +9,7 @@
 project = "gimie"
 copyright = "2023, SDSC-ORD"
 author = "SDSC-ORD"
-release = "0.5.0"
+release = "0.5.1"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
