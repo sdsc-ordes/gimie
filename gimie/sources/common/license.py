5dd462e930ee682282276042d06108068c368be5
rmfranken <77491494+rmfranken@users.noreply.github.com>
2023-09-08 08:28:38 +0200

    [feature] remove security flawed hardcoded github token and use environment variable instead

--- gimie/sources/common/license.py
+++ gimie/sources/common/license.py
@@ -1,123 +1,147 @@
-from tempfile import TemporaryFile
 import os
 import re
-from scancode.api import get_licenses
+from typing import List
+
+from gimie.graph.namespaces import GIMIE
+
+
+import base64
+import os
+import re
+from typing import List
+
+from gimie.graph.namespaces import GIMIE
 import requests
 import json
+github_token = os.environ.get('GITHUB_TOKEN')
+headers = {'Authorization': f'token {github_token}'}
+username = 'rmfranken'
 
-repo_url = "https://github.com/comfyanonymous/ComfyUI"
-github_token = os.environ.get("GITHUB_TOKEN")
-headers = {"Authorization": f"token {github_token}"}
-repo_url = repo_url.rstrip("/")
-
-
-def get_branch_name_and_file_dict(repo_url):
-    url = repo_url.replace(
-        "https://github.com", "https://api.github.com/repos"
-    )
+def get_files_in_repository_root(repo_url):
+    # Extract the username and repository name from the URL
     parts = repo_url.strip("/").split("/")
     username = parts[-2]
     repo_name = parts[-1]
-    url = "https://api.github.com/graphql"
-    query = """
-       query {
-         repository(owner: "%s", name: "%s") {
-           defaultBranchRef {
-             name
-           }
-           object(expression: "HEAD:") {
-             ... on Tree {
-               entries {
-                 name
-               }
-             }
-           }
-         }
-       }
-       """ % (
-        username,
-        repo_name,
-    )
-    # Send a GET request to the GitHub API to get repository information
-    response = requests.post(url, json={"query": query}, headers=headers)
 
-    # Check if the request was successful (status code 200)
-    if response.status_code == 200:
-        repository_info = response.json()
-        default_branch_name = repository_info["data"]["repository"][
-            "defaultBranchRef"
-        ]["name"]
-        files_dict = repository_info["data"]["repository"]["object"]["entries"]
-        try:
-            return default_branch_name, files_dict
-        except KeyError:
-            print("Could not identify default branch")
+    # Construct the GitHub API URL for the repository's contents
+    api_url = f"https://api.github.com/repos/{username}/{repo_name}/contents/"
 
+    try:
+        # Make a GET request to the GitHub API
+        response = requests.get(api_url, headers=headers)
 
-def get_license_path(files_dict, license_files):
-    """Given a list of files, returns the URL filepath which contains the license"""
-    if files_dict:
-        for file in files_dict:
-            if file["name"].startswith("."):
+        # Check if the request was successful (status code 200)
+        if response.status_code == 200:
+            # Parse the JSON response
+            contents = response.json()
+
+            # Filter out only the files (exclude directories)
+            files = [item['name'] for item in contents if item['type'] == 'file']
+
+            return files
+
+        else:
+            # If the request was not successful, raise an exception
+            response.raise_for_status()
+
+    except requests.exceptions.RequestException as e:
+        print(f"Error: {e}")
+        return None
+
+# https://github.com/SDSC-ORD/gimie/blob/main/LICENSE
+# Example usage:
+repo_url = "https://github.com/SDSC-ORD/gimie"
+files_in_root = get_files_in_repository_root(repo_url)
+license_files = []
+pattern = r".*(license(s)?|reus(e|ing)|copy(ing)?)(\.(txt|md|rst))?$"
+def return_license_path(files):
+    if files:
+        for file in files:
+            print(file)
+            if file.startswith("."):
                 continue
-            pattern = (
-                r".*(license(s)?|reus(e|ing)|copy(ing)?)(\.(txt|md|rst))?$"
-            )
-            if re.match(pattern, file["name"], flags=re.IGNORECASE):
-                license_path = (
-                    repo_url
-                    + f"/blob/{get_branch_name_and_file_dict(repo_url)[0]}/"
-                    + file["name"]
-                )
+
+            if re.match(pattern, file, flags=re.IGNORECASE):
+                license_path = repo_url+"/blob/main/"+file
                 license_files.append(license_path)
-                print(
-                    license_path
-                    + " is the license path gimie found, starting extraction of license..."
-                )
+    for license_file in license_files:
+        return license_file
 
-    if len(license_files) > 1:
-        return "More than 1 license file was found, please make sure you only have one license."
-    else:
-        for license_file in license_files:
-            return license_file
+print(return_license_path(get_files_in_repository_root(repo_url)))
 
-
+url = (return_license_path(get_files_in_repository_root(repo_url)))
 def github_read_file(url, github_token=None):
-    """Uses the Github API to download the license file using its URL"""
     headers = {}
     if github_token:
-        headers["Authorization"] = f"token {github_token}"
+        headers['Authorization'] = f"token {github_token}"
 
     r = requests.get(url, headers=headers)
     r.raise_for_status()
     data = r.json()
+    print(data)
     return data
 
 
-def extract_license_string(url):
-    """Runs the spdx license matcher (Scancode-toolkit) against the license_string"""
-    file1 = TemporaryFile(delete=False)
-    file1.close()
-    with open(file1.name, "w", encoding="utf-8") as license_handler:
-        json.dump(github_read_file(url), license_handler)
+my_data = github_read_file(url)
+# json.load(my_data)
+file1= open("myfile.json", 'w', encoding='utf-8')
 
-    found_spdx_license_id = get_licenses(file1.name)[
-        "detected_license_expression_spdx"
-    ]
-    return found_spdx_license_id
+file1.write(str(my_data))
+file1.close()
+
+#     print(json.dump())
+
+
+# def locate_licenses(path: str, recurse: bool = False) -> List[str]:
+#     """Returns valid potential paths to license files in the project.
+#     This uses pattern-matching on file names.
+#
+#     Parameters
+#     ----------
+#     path:
+#         The root path to search for license files.
+#     recurse:
+#         Whether to look into subdirectories.
+#
+#     Returns
+#     -------
+#     license_files:
+#         The list of relative paths (from input path) to files which
+#         potentially contain license text.
+#
+#     Examples
+#     --------
+#     >>> locate_licenses('.')
+#     ['./LICENSE']
+#     """
+#     license_files = []
+#     pattern = r".*(license(s)?|reus(e|ing)|copy(ing)?)(\.(txt|md|rst))?$"
+#
+#     for root, _, files in os.walk(path):
+#         # skip toplevel hidden dirs (e.g. .git/)
+#         subdir = os.path.relpath(root, path)
+#         if subdir.startswith(".") and subdir != ".":
+#             continue
+#         for file in files:
+#             # skip hidden files
+#             if file.startswith("."):
+#                 continue
+#
+#             if re.match(pattern, file, flags=re.IGNORECASE):
+#                 license_path = os.path.join(root, file)
+#                 license_files.append(license_path)
+#
+#         # The first root of os.walk is the current dir
+#         if not recurse:
+#             return license_files
+#
+#     return license_files
 
 
 def get_spdx_url(name: str) -> str:
     """Given an SPDX license identifier, return the full URL."""
-    return f"https://spdx.org/licenses/{name}.html"
+    return f"https://spdx.org/licenses/{name}"
 
-
-def get_license(repo_url, headers, license_files=[]):
-    url = get_license_path(
-        get_branch_name_and_file_dict(repo_url)[1], license_files=license_files
-    )
-    github_read_file(url)
-    print(get_spdx_url(extract_license_string(url)))
-
-
-get_license(repo_url, headers=headers)
+def get_spdx_url(name: str) -> str:
+    """Given an SPDX license identifier, return the full URL."""
+    return f"https://spdx.org/licenses/{name}"
