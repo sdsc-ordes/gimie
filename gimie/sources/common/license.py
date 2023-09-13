2493ba108c61a6651e5401e906d7b311e485ea69
rmfranken <77491494+rmfranken@users.noreply.github.com>
2023-09-13 08:53:02 +0200

    [refactor] Tried to wrap as much as possible into functions, added function documentation and reformatted with Black.

--- gimie/sources/common/license.py
+++ gimie/sources/common/license.py
@@ -16,11 +16,14 @@ from typing import List
 from gimie.graph.namespaces import GIMIE
 import requests
 import json
-github_token = os.environ.get('GITHUB_TOKEN')
-headers = {'Authorization': f'token {github_token}'}
+
+github_token = os.environ.get("GITHUB_TOKEN")
+headers = {"Authorization": f"token {github_token}"}
 repo_url = "https://github.com/SDSC-ORD/gimie"
 
+
 def get_files_in_repository_root(repo_url):
+    """Given a GitHub repository URL, outputs a list of files present in the root folder"""
     # Extract the username and repository name from the URL
     parts = repo_url.strip("/").split("/")
     username = parts[-2]
@@ -39,7 +42,9 @@ def get_files_in_repository_root(repo_url):
             contents = response.json()
 
             # Filter out only the files (exclude directories)
-            files = [item['name'] for item in contents if item['type'] == 'file']
+            files = [
+                item["name"] for item in contents if item["type"] == "file"
+            ]
 
             return files
 
@@ -51,27 +56,39 @@ def get_files_in_repository_root(repo_url):
         print(f"Error: {e}")
         return None
 
+
 files_in_root = get_files_in_repository_root(repo_url)
 license_files = []
 
+
 def return_license_path(files):
+    """Given a list of files, returns the URL filepath which contains the license"""
     if files:
         for file in files:
             if file.startswith("."):
                 continue
-            pattern = r".*(license(s)?|reus(e|ing)|copy(ing)?)(\.(txt|md|rst))?$"
+            pattern = (
+                r".*(license(s)?|reus(e|ing)|copy(ing)?)(\.(txt|md|rst))?$"
+            )
             if re.match(pattern, file, flags=re.IGNORECASE):
-                license_path = repo_url+"/blob/main/"+file
+                license_path = repo_url + "/blob/main/" + file
                 license_files.append(license_path)
-                print(license_path + " is the license path gimie found, starting extraction of license..." )
+                print(
+                    license_path
+                    + " is the license path gimie found, starting extraction of license..."
+                )
     for license_file in license_files:
         return license_file
 
-url = (return_license_path(get_files_in_repository_root(repo_url)))
+
+url = return_license_path(get_files_in_repository_root(repo_url))
+
+
 def github_read_file(url, github_token=None):
+    """Uses the Github API to download the license file using its URL"""
     headers = {}
     if github_token:
-        headers['Authorization'] = f"token {github_token}"
+        headers["Authorization"] = f"token {github_token}"
 
     r = requests.get(url, headers=headers)
     r.raise_for_status()
@@ -79,23 +96,39 @@ def github_read_file(url, github_token=None):
     return data
 
 
-file1= open("myfile.json", 'w', encoding='utf-8')
-json_object1 = json.dump(github_read_file(url),file1)
+file1 = open("myfile.json", "w", encoding="utf-8")
+json_object1 = json.dump(github_read_file(url), file1)
 file1.close()
-# json_object = json.dumps(github_read_file(url))
-with open(r"myfile.json", 'r') as json_object:
-    json_object = json.load(json_object)
-    # print(json_object)
-    license_string = (str(json_object['payload']['blob']['rawLines']))
 
 
-subprocess.run(f'scancode --json-pp ./output.json  --license ./myfile.json ' )
+def extract_license_string():
+    """Runs the spdx license matcher (Scancode-toolkit) against the license_string"""
+    with open(r"myfile.json", "r") as json_object:
+        json_object = json.load(json_object)
+        # print(json_object)
+        license_string = str(json_object["payload"]["blob"]["rawLines"])
+    subprocess.run(
+        f"scancode --json-pp ./output.json  --license ./myfile.json "
+    )
+    os.remove("myfile.json")
 
-with open("output.json", 'r') as file2:
-    json_object2 = json.load(file2)
-    # print(json_object2)
-    license_identifier = json_object2['files'][0]['licenses'][0]['spdx_url']
-    print(license_identifier)
+
+extract_license_string()
+
+
+def extract_spdx_url():
+    """Parses the scancode-toolkit output to extract the full SPDX URL of found license"""
+    with open("output.json", "r") as file2:
+        json_object2 = json.load(file2)
+        license_identifier = json_object2["files"][0]["licenses"][0][
+            "spdx_url"
+        ]
+        print(license_identifier)
+    os.remove("output.json")
+    return license_identifier
+
+
+extract_spdx_url()
 
 
 def get_spdx_url(name: str) -> str:
