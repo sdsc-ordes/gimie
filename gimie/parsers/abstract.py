0e51d62cd3e7371e731a9be5dfa1362b12e90e60
cmdoret <cyril.mattheydoret@gmail.com>
2023-11-03 16:34:10 +0100

    refactor(parser): use bytes -> tuple interface

--- gimie/parsers/abstract.py
+++ gimie/parsers/abstract.py
@@ -15,47 +15,32 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 from abc import ABC, abstractmethod
-from typing import Iterable, Optional
+from functools import reduce
+from typing import Iterable, Set, Tuple, TypeAlias, Union
 
-from rdflib import Graph
-from rdflib.term import URIRef
+from rdflib.term import Literal, URIRef
 
-from gimie.graph.operations import combine_graphs
-from gimie.io import Resource
+Property: TypeAlias = Tuple[URIRef, Union[URIRef, Literal]]
 
 
 class Parser(ABC):
     """Parser is an Abstract Base Class. It is only meant
     to define a standard interface for all parsers.
 
-    All subclasses must implement _parse() and can_parse().
-    A parser receives a subject URI and a resource, and returns
-    a graph of triples using the input URI as subject.
+    All subclasses must implement parse(). A parser parses
+    bytes data into a set of property-object tuples.
     """
 
-    def __init__(self, uri: URIRef, max_size_kb: Optional[int] = 2048):
-        self.max_size_kb = max_size_kb
-        self.uri = uri
+    def __init__(self):
+        pass
 
     @abstractmethod
-    def _parse(self, resource: Resource) -> Graph:
-        """Extract triples"""
+    def parse(self, data: bytes) -> Set[Property]:
+        """Extract property-object tuples from a source."""
         ...
 
-    @abstractmethod
-    def can_parse(self, resource: Resource) -> bool:
-        """Match based on filename (possibly also content and size)"""
-        ...
-
-    def parse(self, resource: Resource) -> Graph:
-        """Parse if parsable, otherwise return an empty graph."""
-        if self.can_parse(resource):
-            return self._parse(resource)
-        return Graph()
-
-    def parse_multiple(self, resources: Iterable[Resource]) -> Graph:
-        """Parse multiple resources and return the union of the graphs.
-        Unparsable resources are ignored."""
-        g = Graph()
-        graphs = map(self.parse, resources)
-        return combine_graphs(*graphs)
+    def parse_all(self, docs: Iterable[bytes]) -> Set[Property]:
+        """Parse multiple sources and return the union of
+        property-object tuples."""
+        properties = map(self.parse, docs)
+        return reduce(lambda p1, p2: p1 | p2, properties)
