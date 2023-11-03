8c6e0cfd9ae859bb3f4c7a9db6d14a3e215468fa
cmdoret <cyril.mattheydoret@gmail.com>
2023-11-03 18:43:10 +0100

    fix(graph): py38 compatible typ alias

--- gimie/graph/__init__.py
+++ gimie/graph/__init__.py
@@ -18,4 +18,4 @@ from typing import Tuple, TypeAlias, Union
 
 from rdflib.term import Literal, URIRef
 
-Property: TypeAlias = Tuple[URIRef, Union[URIRef, Literal]]
+Property = Tuple[URIRef, Union[URIRef, Literal]]
