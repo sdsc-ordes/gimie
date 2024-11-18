f17dd9f6d0ea72c41d52bf7429cd8f474c67ac5a
rmfranken <robin.franken@epfl.ch>
2024-11-18 15:49:09 +0100

    feat: add test for cff author parsing

---
+++ tests/test_cff.py
@@ -0,0 +1,11 @@
+from gimie.io import LocalResource
+from gimie.parsers.cff import get_cff_authors
+
+
+def test_parse_cff():
+    cff_file = LocalResource("CITATION.cff")
+    with open(cff_file.path, "rb") as f:
+        cff_content = f.read()
+    authors = get_cff_authors(cff_content)
+    assert authors is not None
+    assert len(authors) > 0
