4c7a7693e46993f076814f1c48fdf627bad8a7c4
rmfranken <robin.franken.mail@gmail.com>
2026-05-21 16:10:27 +0200

    chore: apply black formatting

--- gimie/extractors/abstract.py
+++ gimie/extractors/abstract.py
@@ -15,6 +15,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 """Abstract for Git repository extractors."""
+
 from abc import ABC, abstractmethod
 from typing import List, Optional
 
