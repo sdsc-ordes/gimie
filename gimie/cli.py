dcdebb43cbf4310bbef679a22f646e5793da8989
rmfranken <robin.franken.mail@gmail.com>
2026-05-21 16:22:24 +0200

    chore: apply black formatting

--- gimie/cli.py
+++ gimie/cli.py
@@ -15,6 +15,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 """Command line interface to the gimie package."""
+
 from enum import Enum
 from typing import List, Optional
 
